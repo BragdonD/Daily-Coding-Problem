d3b948db49efea7dde88a409308476cd6a0cb22a
BragdonD <73294632+BragdonD@users.noreply.github.com>
2023-02-01 19:55:00 +0100

    problem 4

---
+++ problem-4/node/index.js
@@ -0,0 +1,11 @@
+function get_ouput(arr) {
+    arr = arr.filter(elem => elem > 0)
+    arr.sort();
+    for(let i=0; i < arr.length; i++) {
+        if(arr[i] != i+1) return i + 1;
+    }
+    return arr[arr.length - 1] + 1;
+}
+
+console.log(get_ouput([3, 4, -1, 1])) // Expect 2
+console.log(get_ouput([1, 2, 0])) // Expect 3
